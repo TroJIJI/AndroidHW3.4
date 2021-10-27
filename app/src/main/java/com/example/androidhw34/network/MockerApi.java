51288275e0a4a92e28e97f73cbf6ca9896b380c3
TroJIJI <adisrahimov0301@gmail.com>
2021-10-27 18:18:23 +0600

    Initial commit

---
+++ app/src/main/java/com/example/androidhw34/network/MockerApi.java
@@ -0,0 +1,29 @@
+package com.example.androidhw34.network;
+
+import com.example.androidhw34.data.MockerModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface MockerApi {
+    @GET("posts")
+    Call<List<MockerModel>> getPosts();
+
+    @POST("posts")
+    Call<MockerModel> createMocker(@Body MockerModel model);
+
+    @PUT("posts/{id}")
+    Call<MockerModel> update(@Path("id") String id, @Body MockerModel model);
+
+    @DELETE("posts/{id}")
+    Call<MockerModel> deleteMockerModel(
+            @Path("id") Integer id
+    );
+}
